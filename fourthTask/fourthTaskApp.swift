ad8494ae26b74ed2985e12eafbc807f3c4f18f12
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-10-10 12:47:42 +0300

    Initial Commit

---
+++ fourthTask/fourthTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  fourthTaskApp.swift
+//  fourthTask
+//
+//  Created by  Pavel Nepogodin on 10.10.23.
+//
+
+import SwiftUI
+
+@main
+struct fourthTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
